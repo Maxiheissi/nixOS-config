ea624d70f966eca4509d2b2c81ccaa9f3cb0cc67
maxiheissi <mhe@aon.at>
2026-04-17 23:19:40 +0200

    hyprland setup

--- home/desktop-home.nix
+++ home/desktop-home.nix
@@ -18,16 +18,58 @@
         kb_layout = "de";
       };
 
-      "$mod" = "SUPER";
+    "$mod" = "SUPER";
+    "$terminal" = "kitty";
+    "$fileManager" = "yazi";
+    "$menu" = "wofi --show drun";
 
       bind =
       [
-      "$mod, Return, exec, kitty"
+      # Apps
+      "$mod, RETURN, exec, $terminal"
+      "$mod, E, exec, $terminal -e $fileManager"
+      "$mod, R, exec, $menu"
+
+
+
+# Fenster
       "$mod, Q, killactive"
-      "$mod, R, exec, wofi --show drun"
       "$mod, F, fullscreen"
-      
-      "$mod, E, exec, kitty -e yazi"
+      "$mod, V, togglefloating"
+
+      # Fokus bewegen (Pfeiltasten)
+      "$mod, left, movefocus, l"
+      "$mod, right, movefocus, r"
+      "$mod, up, movefocus, u"
+      "$mod, down, movefocus, d"
+
+      # Fenster verschieben
+      "$mod SHIFT, left, movewindow, l"
+      "$mod SHIFT, right, movewindow, r"
+      "$mod SHIFT, up, movewindow, u"
+      "$mod SHIFT, down, movewindow, d"
+
+# Workspaces
+      "$mod, 1, workspace, 1"
+      "$mod, 2, workspace, 2"
+      "$mod, 3, workspace, 3"
+      "$mod, 4, workspace, 4"
+      "$mod, 5, workspace, 5"
+
+      # Fenster verschieben zu Workspace
+      "$mod SHIFT, 1, movetoworkspace, 1"
+      "$mod SHIFT, 2, movetoworkspace, 2"
+      "$mod SHIFT, 3, movetoworkspace, 3"
+      "$mod SHIFT, 4, movetoworkspace, 4"
+      "$mod SHIFT, 5, movetoworkspace, 5"
+
+      # Reload
+      "$mod SHIFT, R, exec, hyprctl reload"
+
+      # Exit
+      "$mod SHIFT, X, exit"
+
+
        ];
       
     };
