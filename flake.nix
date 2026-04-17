455e6e877e6949c8bcff0b8e3bc1535a9fb4ba3a
maxiheissi <mhe@aon.at>
2026-04-17 22:36:21 +0200

    home manager fixed

--- flake.nix
+++ flake.nix
@@ -4,7 +4,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager = {
+	url = "github:nix-community/home-manager";
+	inputs.nixpkgs.follows = "nixpkgs";
+    }
   };
 
   outputs = { nixpkgs, ... }: {
