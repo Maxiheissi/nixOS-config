59421d29711284ef6c7820d0ba60a8d2359420d7
maxiheissi <mhe@aon.at>
2026-04-18 13:01:25 +0200

    hypr config

--- home/hyprland-home.nix
+++ home/hyprland-home.nix
@@ -26,17 +26,17 @@
         
         "$mod, F, fullscreen"
         # Workspaces
-        "$mod SHIFT, 1, workspace, 1"
-        "$mod SHIFT, 2, workspace, 2"
-        "$mod SHIFT, 3, workspace, 3"
-        "$mod SHIFT, 4, workspace, 4"
-        "$mod SHIFT, 5, workspace, 5"
+        "$mod, SHIFT, 1, workspace, 1"
+        "$mod, SHIFT, 2, workspace, 2"
+        "$mod, SHIFT, 3, workspace, 3"
+        "$mod, SHIFT, 4, workspace, 4"
+        "$mod, SHIFT, 5, workspace, 5"
         # Fenster verschieben
-        "$mod 1, movetoworkspace, 1"
-        "$mod 2, movetoworkspace, 2"
-        "$mod 3, movetoworkspace, 3"
-        "$mod 4, movetoworkspace, 4"
-        "$mod 5, movetoworkspace, 5"
+        "$mod, 1, movetoworkspace, 1"
+        "$mod, 2, movetoworkspace, 2"
+        "$mod, 3, movetoworkspace, 3"
+        "$mod, 4, movetoworkspace, 4"
+        "$mod, 5, movetoworkspace, 5"
 
         
         "ALT, Tab workspace, e+1"
