8950df08faa2b4238f9238f116fe8d38d9769cf2
maxiheissi <mhe@aon.at>
2026-04-18 13:41:17 +0200

    theme set

--- flake.nix
+++ flake.nix
@@ -5,14 +5,16 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-	url = "github:nix-community/home-manager";
-	inputs.nixpkgs.follows = "nixpkgs";
+  	  url = "github:nix-community/home-manager";
+	  inputs.nixpkgs.follows = "nixpkgs";
+	  nix-colors.url = "github:misterio77/nix-colors";
     };    
   };
 
-  outputs = { nixpkgs, home-manager, ... }: {
+  outputs = { nixpkgs, home-manager, nix-colors, ... }: {
     nixosConfigurations.laptop= nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = {inherit inputs;};
       modules = [
         ./hosts/laptop/default.nix
         ./hosts/laptop/hardware.nix
--- home/hosts/laptop.nix
+++ home/hosts/laptop.nix
@@ -1,8 +1,11 @@
 { ... }:
 {
   imports = [
+    inputs.nix-colors.homeManagerModules.default
     ../common-home.nix
     ../desktop-home.nix
     ../hyprland-home.nix
   ];
+
+  colorScheme = inputs.nix-colors.colorSchemes.nord;
 }
