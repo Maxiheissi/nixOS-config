2dacbe094be097ffc0f25882883434a3178231fe
maxiheissi <mhe@aon.at>
2026-04-18 15:14:42 +0200

    theme yazi

--- home/common-home.nix
+++ home/common-home.nix
@@ -46,14 +46,21 @@
         use = "kanagawa";
       };
       manager = {
-        border_style = "solid";
+        border_style = "plain";
       };
       
       indicator = {
         padding = 
           { open = "▐"; close = "▌"; };
         };
-            
+       mode = {
+          normal_main = { padding = { open = "▐"; close = "▌"; }; };
+          normal_alt = { padding = { open = "▐"; close = "▌"; }; };
+          select_main = { padding = { open = "▐"; close = "▌"; }; };
+          select_alt = { padding = { open = "▐"; close = "▌"; }; };
+          unset_main = { padding = { open = "▐"; close = "▌"; }; };
+          unset_alt = { padding = { open = "▐"; close = "▌"; }; };
+  };     
     };
  };  
 
