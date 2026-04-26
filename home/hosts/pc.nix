e7945bd92132d6544e636f08b716be1318185c85
maxiheissi <mhe@aon.at>
2026-04-26 19:26:04 +0200

    fixed moitor position

--- home/hosts/pc.nix
+++ home/hosts/pc.nix
@@ -7,8 +7,8 @@
   ];
 
  wayland.windowManager.hyprland.settings.monitor = [
-    "HDMI-A-1,1920x1080@60,0x0,1"
-    "DP-1,1920x1080@60,1920x0,1"
+    "HDMI-A-1,1920x1080@60,1920x0,1"
+    "DP-2,1920x1080@60,0x0,1"
   ];
   programs.waybar.style = lib.mkAfter ''
     * {
